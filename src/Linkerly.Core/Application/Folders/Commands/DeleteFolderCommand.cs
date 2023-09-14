a25e540a097543d061d4e11d73f60cb71823c4b4
Marek Olšák <olsak.marek@outlook.cz>
2023-09-14 23:17:00 +0200

    Added commands, queries, their mapping configurations and validators for the folder entity data model #3

---
+++ src/Linkerly.Core/Application/Folders/Commands/DeleteFolderCommand.cs
@@ -0,0 +1,48 @@
+﻿using Linkerly.Data;
+using Linkerly.Domain.Application.Models;
+using Linkerly.Domain.Commands;
+using Linkerly.Domain.Exceptions;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Linkerly.Core.Application.Folders.Commands;
+
+public class DeleteFolderCommand : ICommand<Unit>
+{
+	public DeleteFolderCommand(int folderId)
+	{
+		FolderID = folderId;
+	}
+
+	public int FolderID { get; }
+}
+
+public class DeleteFolderCommandHandler : ICommandHandler<DeleteFolderCommand, Unit>
+{
+	private readonly CloudContext _databaseContext;
+
+	public DeleteFolderCommandHandler(CloudContext databaseContext)
+	{
+		_databaseContext = databaseContext;
+	}
+
+	public Task<Unit> Handle(DeleteFolderCommand request, CancellationToken cancellationToken)
+	{
+		ArgumentNullException.ThrowIfNull(request);
+
+		cancellationToken.ThrowIfCancellationRequested();
+
+		var originalFolder = _databaseContext.Folders.AsTracking().SingleOrDefault(folder => folder.FolderID == request.FolderID);
+
+		if (originalFolder is null)
+		{
+			throw new EntityNotFoundException(request.FolderID.ToString(), nameof(FolderEntity));
+		}
+
+		originalFolder.IsActive = false;
+
+		_ = _databaseContext.SaveChanges();
+
+		return Unit.Task;
+	}
+}
---
+++ src/Linkerly.Core/Application/Folders/Commands/UpdateFolderCommand.cs
@@ -0,0 +1,63 @@
+﻿using AutoMapper;
+using Linkerly.Data;
+using Linkerly.Domain.Application.Models;
+using Linkerly.Domain.Exceptions;
+using Linkerly.Domain.Queries;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Linkerly.Core.Application.Folders.Commands;
+
+public class UpdateFolderCommand : IQuery<Unit>
+{
+	public UpdateFolderCommand(int folderId, int userId, int? parentId, int typeId, string? name, long totalSize, long totalCount)
+	{
+		FolderID = folderId;
+		UserID = userId;
+		ParentID = parentId;
+		TypeID = typeId;
+		Name = name;
+		TotalSize = totalSize;
+		TotalCount = totalCount;
+	}
+
+	public int FolderID { get; }
+	public int UserID { get; }
+	public int? ParentID { get; }
+	public int TypeID { get; }
+	public string? Name { get; }
+	public long TotalSize { get; }
+	public long TotalCount { get; }
+}
+
+public class UpdateFolderCommandHandler : IQueryHandler<UpdateFolderCommand, Unit>
+{
+	private readonly CloudContext _databaseContext;
+	private readonly IMapper _mapper;
+
+	public UpdateFolderCommandHandler(CloudContext databaseContext, IMapper mapper)
+	{
+		_databaseContext = databaseContext;
+		_mapper = mapper;
+	}
+
+	public Task<Unit> Handle(UpdateFolderCommand request, CancellationToken cancellationToken)
+	{
+		ArgumentNullException.ThrowIfNull(request);
+
+		cancellationToken.ThrowIfCancellationRequested();
+
+		var originalFolder = _databaseContext.Folders.AsTracking().SingleOrDefault(folder => folder.FolderID == request.FolderID);
+
+		if (originalFolder is null)
+		{
+			throw new EntityNotFoundException(request.FolderID.ToString(), nameof(FolderEntity));
+		}
+
+		_ = _mapper.Map(request, originalFolder);
+
+		_ = _databaseContext.SaveChanges();
+
+		return Unit.Task;
+	}
+}
