d30da9caa5b4ec91c329781e950d7bcbbb6ed74d
Marek Olšák <olsak.marek@outlook.cz>
2023-09-15 02:20:31 +0200

    Added main navigation bar and side bar #5

--- src/Linkerly.Web/Program.cs
+++ src/Linkerly.Web/Program.cs
@@ -1,11 +1,14 @@
 using Linkerly.Core.Application.Users.Mappings;
 using Linkerly.Domain.Application.Mappings;
 using Linkerly.Web;
+using MudBlazor.Services;
 
 var applicationBuilder = WebApplication.CreateBuilder(args);
 
 applicationBuilder.Services.AddRazorPages();
 applicationBuilder.Services.AddServerSideBlazor();
+
+applicationBuilder.Services.AddMudServices();
 
 applicationBuilder.Services.AddSqlite(applicationBuilder.Configuration, applicationBuilder.Environment);
 applicationBuilder.Services.AddAutoMapper(typeof(UserEntityMappingConfiguration), typeof(UserCommandMappingConfiguration));
