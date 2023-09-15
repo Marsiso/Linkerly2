94bd9932f9c230af9d2c25d1a959e488deaf16e3
Marek Olšák <olsak.marek@outlook.cz>
2023-09-15 03:12:58 +0200

    Added main layout view model #5

---
+++ src/Linkerly.Application/ViewModels/PageComponentBase.cs
@@ -0,0 +1,47 @@
+﻿using System.ComponentModel;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Linkerly.Application.ViewModels;
+
+public class PageComponentBase<TViewModel> : ComponentBase where TViewModel : ViewModelBase
+{
+	[FromServices] public required TViewModel Model { get; set; }
+
+	public void Dispose()
+	{
+		Model.PropertyChanged -= OnModelPropertyChanged;
+	}
+
+	protected override bool ShouldRender()
+	{
+		return Model.Busy is false;
+	}
+
+	protected override Task OnInitializedAsync()
+	{
+		Model.PropertyChanged += OnModelPropertyChanged;
+
+		return Model.OnViewModelInitialized();
+	}
+
+	protected override Task OnParametersSetAsync()
+	{
+		return Model.OnViewModelParametersSet();
+	}
+
+	protected override Task OnAfterRenderAsync(bool firstRender)
+	{
+		if (firstRender)
+		{
+			return Model.OnViewModelAfterRender();
+		}
+
+		return base.OnAfterRenderAsync(firstRender);
+	}
+
+	private async void OnModelPropertyChanged(object? sender, PropertyChangedEventArgs args)
+	{
+		await InvokeAsync(StateHasChanged);
+	}
+}
---
+++ src/Linkerly.Application/ViewModels/PageLayout.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Linkerly.Application.ViewModels;
+
+public class PageLayout : LayoutComponentBase
+{
+	public bool IsSidebarVisible { get; set; }
+
+	public void ToggleSidebar()
+	{
+		IsSidebarVisible = !IsSidebarVisible;
+	}
+}
